82a0c76ac40c6f37e33e937038ad57f1610ec077
chinedu <crankyekeruche@gmail.com>
2022-11-17 03:12:44 -0600

    init method added

--- asset_handler.py
+++ asset_handler.py
@@ -1,7 +1,20 @@
-from dataclasses import dataclass
+import random
+from itertools import permutations
 
 
 
-@dataclass
+
 class AssetHandler:
-    ...
+    markets = []
+    
+    def __init__(self, num_assets: int, eval_metric: str, num_markets: int = 5):
+        assert eval_metric in ["volume"]
+        self.num_assets = num_assets
+        self.selected_markets = random.choices(self.markets, k=num_markets)
+  
+    def __next__(self) -> list[str]:
+        ...
+        
+    def load_data(self):
+        ...
+        
