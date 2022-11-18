3e7e4820c41bfbede807c1d8fe7dcf02b1a802cf
chinedu <crankyekeruche@gmail.com>
2022-11-18 14:50:40 -0600

    added cnn policy

--- models/agent.py
+++ models/agent.py
@@ -1,6 +1,6 @@
 import tensorflow as tf
 from keras import backend as K 
-from keras.layers import LeakyReLU, Dense, Conv1D, Input, Dropout, Conv2D, MaxPooling1D, LSTM, BatchNormalization, Bidirectional, Flatten, Concatenate
+from keras.layers import LeakyReLU, Dense, Conv1D, Input, Dropout, Conv2D, Reshape, LSTM, BatchNormalization, Bidirectional, Flatten, Concatenate
 from keras.optimizers import Adam
 from keras.models import Model
 import numpy as np
@@ -8,10 +8,12 @@ from noise import OUActionNoise
 from buffer import MemoryBuffer
 
 
-class LSTMAgent:
+class DDPGAgent:
     def __init__(self, window_length: int, n_stocks: int, actor_lr: float, critic_lr: float,
-                 tau: float, discount_rate: float, buffer_capacity: int = 1_000_000, batch_size: int = 32):
+                 tau: float, discount_rate: float, buffer_capacity: int, batch_size: int, policy: str):
         
+        assert policy in ["CNN", "LSTM"]
+        self.policy = policy
         self.window_length = window_length
         self.n_stocks = n_stocks
         self.actor_lr = actor_lr
@@ -37,11 +39,17 @@ class LSTMAgent:
         #------ Starting stream 1-----#
         input1 = Input(shape=(self.window_length, self.n_stocks*4))
         
-        s1 = Bidirectional(LSTM(32))(input1)
-        
+        if self.policy == "LSTM":
+            s1 = Bidirectional(LSTM(32))(input1)
+        else:
+            resh = Reshape(target_shape=(self.window_length, self.n_stocks, 4))(input1)
+            s1 = Conv2D(32, kernel_size=3, strides=2)(resh)
+            
+        s1 = LeakyReLU(0.2)(s1)
+        s1 = Flatten()(s1)
         #------ Starting stream 2------#
         
-        input2 = Input(shape=(self.n_stocks, self.n_stocks))
+        input2 = Input(shape=(self.n_stocks*4, self.n_stocks*4))
         
         s2 = Conv1D(16, kernel_size=3, strides=1)(input2)
         s2 = LeakyReLU(0.2)(s2)
@@ -65,10 +73,17 @@ class LSTMAgent:
         #------ Starting stream 1-----#
         input1 = Input(shape=(self.window_length, self.n_stocks*4))
         
-        s1 = Bidirectional(LSTM(32))(input1)
+        if self.policy == "LSTM":
+            s1 = Bidirectional(LSTM(32))(input1)
+        else:
+            resh = Reshape(target_shape=(self.window_length, self.n_stocks, 4))(input1)
+            s1 = Conv2D(32, kernel_size=3, strides=2)(resh)
+            
+        s1 = LeakyReLU(0.2)(s1)
+        s1 = Flatten()(s1)
         
         #------ Starting stream 2------#
-        input2 = Input(shape=(self.n_stocks, self.n_stocks))
+        input2 = Input(shape=(self.n_stocks*4, self.n_stocks*4))
         
         s2 = Conv1D(25, kernel_size=3, strides=1)(input2)
         s2 = LeakyReLU(0.2)(s2)
@@ -99,25 +114,37 @@ class LSTMAgent:
         return action_
     
     def learn(self):
+        if (self.buffer.size() <= self.batch_size): return
         # sample from buffer
         states, actions, rewards, dones, new_states, idx = self.sample_batch(self.batch_size)
         
         # get target q-value using target network
-        q_vals = self.critic_target([new_states, self.actor_target(new_states)])
+
+        q_vals = self.critic_target([*new_states, self.actor_target(new_states)])
         
         # bellman iteration for target critic value
-        critic_target = np.asarray(q_vals)
+        critic_target = np.array(q_vals)
         for i in range(q_vals.shape[0]):
             if dones[i]:
                 critic_target[i] = rewards[i]
             else:
                 critic_target[i] = self.gamma * q_vals[i] + rewards[i]
                 
-            if self.with_per:
-                self.buffer.update(idx[i], abs(q_vals[i]-critic_target[i]))
+            self.buffer.update(idx[i], abs(q_vals[i]-critic_target[i])[0])
                 
         # train(or update) the actor & critic and target networks
         self.update_networks(states, actions, critic_target)
+        
+    def memorize(self, obs, action, reward, done, new_obs):
+        act = self.actor.predict(obs)
+        qval = self.critic([*obs, act])
+        
+        next_act = self.actor.predict(new_obs)
+        next_q_val = self.critic_target.predict([*new_obs, next_act])
+        new_val = reward + self.gamma*next_q_val
+        td_error = abs(new_val - qval)[0]
+        
+        self.buffer.memorize(*obs, action, reward, done, *new_obs, td_error)
     
     def Qgradient(self, obs, acts):
         acts = tf.convert_to_tensor(acts)
@@ -159,7 +186,7 @@ class LSTMAgent:
         
     def train_critic(self, obs: list, acts, target):
         with tf.GradientTape() as tape:
-            q_values = self.critic([obs, acts], training=True)
+            q_values = self.critic([*obs, acts], training=True)
             td_error = q_values - target
             critic_loss = tf.reduce_mean(tf.math.square(td_error))
             self.critic_loss = float(critic_loss)
@@ -169,14 +196,22 @@ class LSTMAgent:
     def train_actor(self, obs: list, critic):
         with tf.GradientTape() as tape:
             actions = self.actor(obs)
-            actor_loss = -tf.reduce_mean(critic([obs,actions]))
+            actor_loss = -tf.reduce_mean(critic([*obs,actions]))
         actor_grad = tape.gradient(actor_loss,self.actor.trainable_variables)
         self.actor_optimizer.apply_gradients(zip(actor_grad,self.actor.trainable_variables))
         
     def sample_batch(self, batch_size: int):
         """ Sampling from the batch
         """
-        return self.buffer.sample_batch(batch_size)
+        s1, s2, s3, a, r, d, ns1, ns2, ns3, idx =  self.buffer.sample_batch(batch_size)
+        s1 = tf.convert_to_tensor(s1.reshape(s1.shape[0], self.window_length, self.n_stocks*4))
+        s2 = tf.convert_to_tensor(s2.reshape(s2.shape[0], self.n_stocks*4, self.n_stocks*4))
+        s3 = tf.convert_to_tensor(s3.reshape(s3.shape[0], self.n_stocks))
+        
+        ns1 = tf.convert_to_tensor(ns1.reshape(ns1.shape[0], self.window_length, self.n_stocks*4))
+        ns2 = tf.convert_to_tensor(ns2.reshape(ns2.shape[0], self.n_stocks*4, self.n_stocks*4))
+        ns3 = tf.convert_to_tensor(ns3.reshape(ns3.shape[0], self.n_stocks))
+        return [s1, s2, s3], a, r, d, [ns1, ns2, ns3], idx
         
     def save_weights(self, paths: list[str]):
         assert len(paths) == 4
