628fa4c2fb8e931f0a74d4cd04467691ccb35a6d
chinedu <crankyekeruche@gmail.com>
2022-11-21 00:44:34 -0600

    args

--- train.py
+++ train.py
@@ -9,40 +9,43 @@ import matplotlib.pyplot as plt
 
 
 from models import DDPGAgent
-from environment import StockEnv, Portfolio
+from environment import StockEnv, Portfolio, DataHandler
 
 
 def parse_args():
     parser = argparse.ArgumentParser("DDPG algorithm for portfolio allocation")
     # Environment and Portfolio
-    parser.add_argument("--seed", type=int, default=42, help="which seed to use")
+    parser.add_argument("--seed", type=int, default=32, help="which seed to use")
     parser.add_argument("--initial-amount", type=int, default=10_000, help="Starting amount for the portfolio")
     parser.add_argument("--mode", type=str, default="train", help="training or testing")
-    parser.add_argument("--companies", type=list[str], default=["GOOGL", "AAPL", "MSFT", "ZION", "COST", "IBM"],
-                        help="Companies to be chosen in portfolio")
-    parser.add_argument("--window", type=int, default=14, help="Lookback window (in days) to be given to agent")
-    parser.add_argument("--train_split", type=float, default=0.8, help="proportion of data to be used for training")
+    parser.add_argument("--companies", type=list[str], default=["BRK-A", "UAL", "NVDA", "AAPL", "NKE", "WMT", "XOM", "PG", "C", "ORCL", "GILD",
+                                                            "HON", "JPM", "INTU", "AXP"], help="Companies to be chosen in portfolio")
+    parser.add_argument("--window", type=int, default=30, help="Lookback window (in days) to be given to agent")
+    parser.add_argument("--train-date", type=str, default="2007-01-05", help="Starting Date to be used for training")
+    parser.add_argument("--test-date", type=str, default="2019-08-06", help="Starting Date to be used for testing")
     
     # Core Algorithm parameters
-    parser.add_argument("--policy", type=str, default="LSTM", help="one of CNN or LSTM policy")
-    parser.add_argument("--buffer-size", type=int, default=10_000, help="replay buffer size")
+    parser.add_argument("--policy", type=str, default="CNN", help="one of CNN or LSTM policy")
+    parser.add_argument("--buffer-size", type=int, default=100_000, help="replay buffer size")
     parser.add_argument("--actor-lr", type=float, default=3e-2, help="actor learning rate for Adam optimizer")
     parser.add_argument("--critic-lr", type=float, default=1e-2, help="critic learning rate for Adam optimizer")
-    parser.add_argument("--tau", type=float, default=1e-4, help="which seed to use")
-    parser.add_argument("--discount-rate", type=float, default=0.995,
+    parser.add_argument("--tau", type=float, default=0.005, help="which seed to use")
+    parser.add_argument("--discount-rate", type=float, default=0.95,
                         help="gamma value for discounted rewards")
-    parser.add_argument("--epsilon-min", type=float, default=0.1,
+    parser.add_argument("--epsilon-min", type=float, default=0.15,
                         help="minimum epsilon value for exploration")
-    parser.add_argument("--epsilon", type=float, default=0.9,
+    parser.add_argument("--epsilon", type=float, default=0.95,
                         help="starting epsilon value for exploration")
-    parser.add_argument("--epsilon-decay", type=float, default=0.1,
+    parser.add_argument("--epsilon-decay", type=float, default=0.025,
                         help="epsilon decay value for exploration")
  
     parser.add_argument("--num-epochs", type=int, default=100,
                         help="total number of episodes to run the environment for")
     parser.add_argument("--batch-size", type=int, default=32,
                         help="number of transitions to optimize at the same time")
-    parser.add_argument("--learning-freq", type=int, default=10,
+    parser.add_argument("--learning-freq", type=int, default=25,
+                        help="number of iterations between every optimization step")
+    parser.add_argument("--target-update-freq", type=int, default=500,
                         help="number of iterations between every optimization step")
     parser.add_argument("--start-epoch", type=int, default=10,
                         help="number of epochs to pass before training starts")
@@ -84,6 +87,11 @@ def main():
                      critic_lr=args.critic_lr, tau=args.tau, discount_rate=args.discount_rate,
                      buffer_capacity=args.buffer_size, batch_size=args.batch_size, policy=args.policy,
                      mode=args.mode, epsilon=args.epsilon, epsilon_decay=args.epsilon_decay, epsilon_min=args.epsilon_min)
+    print(agent.actor.summary())
+    print(agent.critic.summary())
+    
+    end = datetime.datetime.now().strftime("%Y-%m-%d")
+    data_handler = DataHandler(companies=args.companies, start=args.train_date, end=end, test_start=args.test_date)
     
     if args.load_on_start:
         try:
@@ -92,13 +100,11 @@ def main():
             print("Could not find older models")
     max_len = 3
     all_rewards = deque(maxlen=max_len)
-    actor_losses = []
-    critic_losses = []
     portfolios = deque(maxlen=max_len)
     t = 0
     for epoch in range(args.num_epochs):
-        portfolio = Portfolio(asset_names=args.companies, amount=args.initial_amount, weights=init_weights
-                          ,window=args.window, train_split=args.train_split, mode=args.mode)
+        portfolio = Portfolio(asset_names=args.companies, amount=args.initial_amount, weights=init_weights, data_handler=data_handler,
+                          window=args.window, mode=args.mode, start_date=args.train_date, test_date=args.test_date)
     
         env = StockEnv(portfolio, mode=args.mode)
         state = env.reset()
@@ -117,6 +123,9 @@ def main():
                 tf.summary.scalar('actor loss', data=actor_loss, step=t)
                 tf.summary.scalar('critic loss', data=critic_loss, step=t)
             
+            if t%args.target_update_freq == 0:
+                agent.update_target_networks()
+                
             state = nstate
             rewards.append(reward)
             t += 1
@@ -128,14 +137,9 @@ def main():
         all_rewards.append(rewards)
         portfolios.append(portfolio)
         
-    #for rewards in all_rewards:
-    #    plt.plot(rewards)
-    #    plt.show()
     for portfolio in portfolios:
         portfolio.plot()
-    #plt.plot(actor_losses)
-    #plt.plot(critic_losses)
-    #plt.show()
+
 
     
 if __name__ == "__main__":
